8d667826b964b7b84774d0c4f5447a484311a90c
Rohithkrishna245 <122268113+Rohithkrishna245@users.noreply.github.com>
2023-02-03 22:52:26 -0800

    Create 2.8.c

---
+++ 2.8.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+main()
+{
+	int a=2,b=2,c,d,e;
+	int i[2][2],j[2][2],k[2][2];
+	printf("Enter The Fist matrix Elements:");
+	for(c=0;c<a;c++)
+	{
+		for(d=0;d<b;d++)
+		{
+			scanf("%d",&i[c][d]);
+		}
+	}
+	printf("Enter The Second Matrix Elements:");
+	for(c=0;c<a;c++)
+	{
+		for(d=0;d<b;d++)
+		{
+			scanf("%d",&j[c][d]);
+		}
+	}
+	for(c=0;c<a;c++)
+	{
+		for(d=0;d<b;d++)
+		{
+			k[c][d]=i[c][d]+j[c][d];
+		}
+	}
+	for(c=0;c<a;c++)
+	{
+		for(d=0;d<b;d++)
+		{
+			printf("%d\t",k[c][d]);
+		}
+		printf("\n");
+	}
+}
