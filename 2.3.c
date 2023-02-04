131f0c1784cb5b6beb95dd3649e4a9cb2f25ed99
Rohithkrishna245 <122268113+Rohithkrishna245@users.noreply.github.com>
2023-02-03 22:49:49 -0800

    Create 2.3.c

---
+++ 2.3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main()
+{
+	int a,b,c,d;
+	printf("Enter the Number:");
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+		printf("The Number %d is even",a);
+	}
+	else
+	{
+		printf("The Number %d is Odd",a);
+	}
+}
